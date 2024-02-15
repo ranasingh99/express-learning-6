7660402aefd37ea92a6bb2444ba3f470a652dddb
ranasingh99 <sanketverma852@gmail.com>
2024-02-15 17:01:51 +0530

    performing read opeartion using GET method

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 // creating basic server 
 
 const express = require("express");
+const path = require("path");
 
-const port = 3000;
 
-const app = express();// basic server
+const app = express();
 
-app.listen(port,`server is listening at port ${port}`);
+// get method = read operation (by using url we can read data)
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+// post methode = create opeartion (by using url we can send/create the data)
+// app.post('api/v1/rana',(req,res)=>{
+//     //req.body -- we can send the data whatever we have filled
+   
+// })
+app.listen(3000);
