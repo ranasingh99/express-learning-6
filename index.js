e433daa167ce31163eb601e14a6c54ca31e7b387
ranasingh99 <sanketverma852@gmail.com>
2024-02-15 17:32:10 +0530

    added the post method and read the req body

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // creating basic server 
 
+
 const express = require("express");
 const path = require("path");
-
+const bodyParser = require("body-parser");
 
 const app = express();
+
+
+// now use bodyparser middleware
+
+app.use(bodyParser.urlencoded({extended:false}))
+
 
 // get method = read operation (by using url we can read data)
 app.get('/login',(req,res)=>{
@@ -12,8 +19,10 @@ app.get('/login',(req,res)=>{
 })
 
 // post methode = create opeartion (by using url we can send/create the data)
-// app.post('api/v1/rana',(req,res)=>{
-//     //req.body -- we can send the data whatever we have filled
-   
-// })
+app.post('/api/v1/loginform',(req,res)=>{
+    res.send('<h1>Done data is sent to the api</h1>')
+    /*now accessing the data which is sent to the given url
+    for that we will read body and for reading body we need body-parser*/
+    console.log(req.body);
+})
 app.listen(3000);
